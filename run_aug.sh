c8c528d3c568b5d57262fe66ab72e139d4887ef5
philip <philip.yijie.zhang@gmail.com>
2023-05-15 00:53:01 +0200

    update run

--- run_aug.sh
+++ run_aug.sh
@@ -3,27 +3,9 @@
 Ts=(2.0 1.0 0.667 0.5 0.4 0.333 0.286)
 
 for T in ${Ts[*]}; do 
-    echo $T
-    python experiments/train_lik.py --temperature=$T \
-        --augment=False \
-        --replacement=False \
-        --perm=False \
-        --seed=15 \
-        --dataset="mnist" \
-        --data_dir="mnist" \
-        --dirty_lik="lenet" \
-        --likelihood="softmax" \
-        --label_noise=0.0 \
-        --logits_temp=1.0 \
-        --noise=0.0 \
-        --prior-scale=1.0 \
-        --sgld-epochs=100 \
-        --sgld-lr=1e-6 \
-        --momentum=0.99 \
-        --n-samples=10 \
-        --n_cycles=10 &  
+    echo $T 
 
-    python experiments/train_lik.py --temperature=$T \
+    python experiments/train_lik.py --likelihood_temp=$T \
         --augment=True \
         --replacement=False \
         --perm=False \
@@ -42,7 +24,7 @@ for T in ${Ts[*]}; do
         --n-samples=10 \
         --n_cycles=10 &  
 
-    python experiments/train_lik.py --temperature=$T \
+    python experiments/train_lik.py --likelihood_temp=$T \
         --augment=False \
         --replacement=True \
         --perm=False \
@@ -61,7 +43,7 @@ for T in ${Ts[*]}; do
         --n-samples=10 \
         --n_cycles=10 & 
 
-    python experiments/train_lik.py --temperature=$T \
+    python experiments/train_lik.py --likelihood_temp=$T \
         --augment=False \
         --replacement=False \
         --perm=True \
