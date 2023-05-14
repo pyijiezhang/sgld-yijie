102a508ffc38fa0a980535482b56d6673a493627
philip <philip.yijie.zhang@gmail.com>
2023-05-14 14:59:28 +0200

    run

--- run_mlp.sh
+++ run_mlp.sh
@@ -1,17 +1,15 @@
-#!/bin/bash
-
 Ts=(2.0 1.0 0.667 0.5 0.4 0.333 0.286)
 
 for T in ${Ts[*]}; do 
-    echo $T  
+    echo $T
     python experiments/train_lik.py --temperature=$T \
         --augment=False \
-        --replacement=True \
+        --replacement=False \
         --perm=False \
         --seed=15 \
         --dataset="mnist" \
         --data_dir="mnist" \
-        --dirty_lik="lenet" \
+        --dirty_lik="mlp" \
         --likelihood="softmax" \
         --label_noise=0.0 \
         --logits_temp=1.0 \
@@ -21,5 +19,5 @@ for T in ${Ts[*]}; do
         --sgld-lr=1e-6 \
         --momentum=0.99 \
         --n-samples=10 \
-        --n_cycles=10 & 
-done 
+        --n_cycles=10 &
+done
